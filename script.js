8568277a96e4469d29911f2c175c85d7c378d663
Ilhom Abdulahatov <abdulahatov03gmail.com>
2022-12-25 18:00:53 +0300

    operators

---
+++ script.js
@@ -0,0 +1,74 @@
+// Arifmetik operator
+
+// let x=7;
+// let y=3;
+// console.log(x,y);
+// console.log(x+y);
+// console.log(x-y);
+// console.log(x*y);
+// console.log(x/y);
+// console.log(x%y);
+// console.log(x**y);
+
+// console.log(++x);
+// console.log(x++);
+// console.log(x);
+
+// \\\
+
+// Solishtiruv operator
+// let x = 1;
+// console.log(x>=1);
+// console.log(x<1);
+// console.log(x<=1);
+// console.log(x===1);
+// console.log(x!==1);
+
+// x=10;
+// y=5;
+// console.log(x+y);
+// x = 5;
+// console.log(x!=='5');
+// console.log('2'=='10');
+
+// ternary operator
+// let points = 70;
+// let result = points > 60 ? `o'tdi`: `o'tnadi`;
+// console.log(result);
+// let x = 10;
+// let result = x == 10 ? `chegirma 20%` : `chergirma 10%`;
+// console.log(result);
+
+// Mantiqiy operator
+// console.log(true && false);
+// console.log(true || false); 
+
+// practice
+// let color1 = `oq`;
+// let color2 = `qora`;
+// let renderColor = color1;
+// color1 = color2;
+// color2 =renderColor
+// color2=color1
+
+// console.log(color1, color2);
+
+// let age = 18;
+// let start = 12;
+// let end = 20;
+// let result = age > start && age < end ? 'Ruxsat' : 'taqiqlanadi';
+// console.log(result);
+
+// let hour = 5;
+
+// if (hour > 6 && hour <= 12){
+//     console.log('good morning');
+// } else if(hour>12 && hour<= 17){
+//     console.log('good afternoon');
+// } else if(hour>17 && hour<= 19){
+//     console.log('good evening');
+// } else if(hour>19 && hour< 24){
+//     console.log('good night');
+// } else{
+//     console.log('notogri soat kiritildi');
+// }
